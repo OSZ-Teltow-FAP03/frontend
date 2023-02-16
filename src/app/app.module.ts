25d3b34173ccbccbad4d003b382ed268b175c908
Marco Krause <marco.krause@fiberboard.de>
2023-02-16 14:58:13 +0100

    added localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { NotFoundComponent } from './error/not-found/not-found.component';
 import { GenericErrorComponent } from './error/generic-error/generic-error.component';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,6 +17,10 @@ import { RegisterComponent } from './login-page/register/register.component';
 import { SendPasswordResetComponent } from './login-page/send-password-reset/send-password-reset.component';
 import { LoginComponent } from './login-page/login/login.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+import localeDeExtra from '@angular/common/locales/extra/de';
+registerLocaleData(localeDe, 'de-DE', localeDeExtra);
 
 @NgModule({
   declarations: [
@@ -40,7 +44,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     ReactiveFormsModule,
     MatToolbarModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-DE' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

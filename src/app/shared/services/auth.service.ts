66c2178c6a31c379506f749d1e981354c0df1829
Marco Krause <marcocoswig97@gmail.com>
2023-02-22 12:32:37 +0100

    changed auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,32 +1,83 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, of, tap } from 'rxjs';
+import { API_TOKEN } from '../api-token';
+import { Login } from '../interfaces/auth';
+import { User } from '../interfaces/user';
+import { PROD_TOKEN } from '../production';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  canLogin = new BehaviorSubject(true);
+  constructor(
+    private readonly http: HttpClient,
+    @Inject(API_TOKEN) private readonly api: string,
+    @Inject(PROD_TOKEN) private readonly prod: boolean,
+    private readonly router: Router
+  ) {}
+
+  authApi = `${this.api}/auth`;
+
+  loggedInUser: User | null = null;
 
   loggedInBool = new BehaviorSubject(false);
 
-  set loggedIn(value: boolean) {
-    this.loggedInBool.next(value);
+  login(login: Login) {
+    if (this.prod === false) {
+      this.loggedInUser = {
+        name: 'Jochen',
+        lastname: 'Schweizer',
+        email: 'email@email.com',
+        password: login.password,
+        username: login.username,
+      };
+      this.loggedInBool.next(true);
+      return of(true);
+    }
+    return this.http.post<boolean>(`${this.authApi}/login`, login).pipe(
+      tap(bool => {
+        this.loggedInBool.next(bool);
+      })
+    );
   }
-
-  login(username: string, password: string) {
-    console.log(username, password);
-    return this.canLogin.asObservable();
-  }
-  register() {
-    console.log('registrieren');
+  register(user: User) {
+    if (this.prod === false) {
+      return of(user);
+    }
+    return this.http.post<User>(`${this.authApi}/register`, user);
   }
   logout() {
-    console.log('login');
+    if (this.prod === false) {
+      return of(true).pipe(
+        tap(() => {
+          console.log('User ausgeloggt');
+          this.router.navigate(['/login']);
+          this.loggedInBool.next(false);
+        })
+      );
+    }
+    return this.http.get<boolean>(`${this.authApi}/logout`).pipe(
+      tap(() => {
+        this.loggedInBool.next(false);
+        this.router.navigate(['/login']);
+      })
+    );
   }
-  passwordReset() {
-    console.log('login');
+  passwordReset(password: string) {
+    if (this.prod === false) {
+      console.log('reset password');
+      return of(true);
+    }
+    return this.http.post<boolean>(`${this.authApi}/reset`, password);
   }
-  sendEmail() {
-    console.log('login');
+
+  sendResetEmail(email: string) {
+    if (this.prod === false) {
+      console.log('Email zum Passwort zurücksetzten geschickt.');
+      return of(true);
+    }
+    return this.http.post<boolean>(`${this.authApi}/send-email`, email);
   }
 }

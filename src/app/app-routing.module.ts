96a75fed123eae9a712631e179454f972e032db6
Marco Krause <marcocoswig97@gmail.com>
2023-02-23 10:53:19 +0100

    addded route for user-management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ArchivePageComponent } from './archive-page/archive-page.component';
 import { VideoPlayerPageComponent } from './video-player-page/video-player-page.component';
+import { UserManagementPageComponent } from './user-management-page/user-management-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'archive', component: ArchivePageComponent },
   { path: 'player', component: VideoPlayerPageComponent },
+  { path: 'users', component: UserManagementPageComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
